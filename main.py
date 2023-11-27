f91ff681549ed8c622432ad2703e3c939f56d659
Zhengming Ding <allanzmding@gmail.com>
2023-11-26 19:30:08 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,65 +1,74 @@
-from collections import defaultdict
+from collections import deque
 from heapq import heappush, heappop 
-from math import sqrt
 
-def prim(graph):
+def shortest_shortest_path(graph, source):
     """
-    ### TODO:
-    Update this method to work when the graph has multiple connected components.
-    Rather than returning a single tree, return a list of trees,
-    one per component, containing the MST for each component.
-
-    Each tree is a set of (weight, node1, node2) tuples.    
-    """
-    def prim_helper(visited, frontier, tree):
-        if len(frontier) == 0:
-            return tree
-        else:
-            weight, node, parent = heappop(frontier)
-            if node in visited:
-                return prim_helper(visited, frontier, tree)
-            else:
-                print('visiting', node)
-                # record this edge in the tree
-                tree.add((weight, node, parent))
-                visited.add(node)
-                for neighbor, w in graph[node]:
-                    heappush(frontier, (w, neighbor, node))    
-                    # compare with dijkstra:
-                    # heappush(frontier, (distance + weight, neighbor))                
-
-                return prim_helper(visited, frontier, tree)
-        
-    # pick first node as source arbitrarily
-    source = list(graph.keys())[0]
-    frontier = []
-    heappush(frontier, (0, source, source))
-    visited = set()  # store the visited nodes (don't need distance anymore)
-    tree = set()
-    prim_helper(visited, frontier, tree)
-    return tree
-
-
-
-def mst_from_points(points):
-    """
-    Return the minimum spanning tree for a list of points, using euclidean distance 
-    as the edge weight between each pair of points.
-    See test_mst_from_points.
-
-    Params:
-      points... a list of tuples (city_name, x-coord, y-coord)
-
+    Params: 
+      graph.....a graph represented as a dict where each key is a vertex
+                and the value is a set of (vertex, weight) tuples (as in the test case)
+      source....the source node
+      
     Returns:
-      a list of edges of the form (weight, node1, node2) indicating the minimum spanning
-      tree connecting the cities in the input.
+      a dict where each key is a vertex and the value is a tuple of
+      (shortest path weight, shortest path number of edges). See test case for example.
+    """
+    ### TODO
+    pass
+    
+def test_shortest_shortest_path():
+
+    graph = {
+                's': {('a', 1), ('c', 4)},
+                'a': {('b', 2)}, # 'a': {'b'},
+                'b': {('c', 1), ('d', 4)}, 
+                'c': {('d', 3)},
+                'd': {},
+                'e': {('d', 0)}
+            }
+    result = shortest_shortest_path(graph, 's')
+    # result has both the weight and number of edges in the shortest shortest path
+    assert result['s'] == (0,0)
+    assert result['a'] == (1,1)
+    assert result['b'] == (3,2)
+    assert result['c'] == (4,1)
+    assert result['d'] == (7,2)
+    
+    
+def bfs_path(graph, source):
+    """
+    Returns:
+      a dict where each key is a vertex and the value is the parent of 
+      that vertex in the shortest path tree.
     """
     ###TODO
     pass
 
-def euclidean_distance(p1, p2):
-    return sqrt((p1[1] - p2[1])**2 + (p1[2] - p2[2])**2)
+def get_sample_graph():
+     return {'s': {'a', 'b'},
+            'a': {'b'},
+            'b': {'c'},
+            'c': {'a', 'd'},
+            'd': {}
+            }
 
+def test_bfs_path():
+    graph = get_sample_graph()
+    parents = bfs_path(graph, 's')
+    assert parents['a'] == 's'
+    assert parents['b'] == 's'    
+    assert parents['c'] == 'b'
+    assert parents['d'] == 'c'
+    
+def get_path(parents, destination):
+    """
+    Returns:
+      The shortest path from the source node to this destination node 
+      (excluding the destination node itself). See test_get_path for an example.
+    """
+    ###TODO
+    pass
 
-
-
+def test_get_path():
+    graph = get_sample_graph()
+    parents = bfs_path(graph, 's')
+    assert get_path(parents, 'd') == 'sbc'
--- test_main.py
+++ test_main.py
@@ -1,44 +1,34 @@
-from main import *
+from main_sol import *
 
+def test_shortest_shortest_path():
 
-def test_prim():    
-    ###TODO
     graph = {
-            's': {('a', 4), ('b', 8)},
-            'a': {('s', 4), ('b', 2), ('c', 5)},
-            'b': {('s', 8), ('a', 2), ('c', 3)}, 
-            'c': {('a', 5), ('b', 3), ('d', 3)},
-            'd': {('c', 3)},
-            'e': {('f', 10)}, # e and f are in a separate component.
-            'f': {('e', 10)}
-        }
-    trees = prim(graph)
-    assert len(trees) == 2
-    # since we are not guaranteed to get the same order
-    # of edges in the answer, we'll check the size and
-    # weight of each tree.
-    len1 = len(trees[0])
-    len2 = len(trees[1])
-    assert min([len1, len2]) == 2
-    assert max([len1, len2]) == 5
-
-    sum1 = sum(e[0] for e in trees[0])
-    sum2 = sum(e[0] for e in trees[1])
-    assert min([sum1, sum2]) == 10
-    assert max([sum1, sum2]) == 12
-    ###    
-
-def test_euclidean_distance():
-    assert round(euclidean_distance(('a', 5, 10), ('b', 7, 12)), 2) == 2.83
+                's': {('a', 1), ('c', 4)},
+                'a': {('b', 2)}, # 'a': {'b'},
+                'b': {('c', 1), ('d', 4)}, 
+                'c': {('d', 3)},
+                'd': {},
+                'e': {('d', 0)}
+            }
+    result = shortest_shortest_path(graph, 's')
+    # result has both the weight and number of edges in the shortest shortest path
+    assert result['s'] == (0,0)
+    assert result['a'] == (1,1)
+    assert result['b'] == (3,2)
+    assert result['c'] == (4,1)
+    assert result['d'] == (7,2)
 
 
-def test_mst_from_points():
-    points = [('a', 5, 10), #(city_name, x-coord, y-coord)
-              ('b', 7, 12),
-              ('c', 2, 3),
-              ('d', 12, 3),
-              ('e', 4, 6),
-              ('f', 6, 7)]
-    tree = mst_from_points(points)
-    # check that the weight of the MST is correct.
-    assert round(sum(e[0] for e in tree), 2) == 19.04
+def test_bfs_path():
+    graph = get_sample_graph()
+    parents = bfs_path(graph, 's')
+    assert parents['a'] == 's'
+    assert parents['b'] == 's'    
+    assert parents['c'] == 'b'
+    assert parents['d'] == 'c'
+
+
+def test_get_path():
+    graph = get_sample_graph()
+    parents = bfs_path(graph, 's')
+    assert get_path(parents, 'd') == 'sbc'
